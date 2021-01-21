0fefa2dcf19fb30178f874b34baa36e86c62f02d
Emma Mendez <emmamendez07@gmail.com>
2021-01-21 13:58:46 +0000

    complete face recognition code

--- src/utilities.js
+++ src/utilities.js
@@ -2644,20 +2644,20 @@ export const TRIANGULATION = [
 ];
 
 // Triangle drawing method
-// const drawPath = (ctx, points, closePath) => {
-//   const region = new Path2D();
-//   region.moveTo(points[0][0], points[0][1]);
-//   for (let i = 1; i < points.length; i++) {
-//     const point = points[i];
-//     region.lineTo(point[0], point[1]);
-//   }
+const drawPath = (ctx, points, closePath) => {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
 
-//   if (closePath) {
-//     region.closePath();
-//   }
-//   ctx.strokeStyle = "grey";
-//   ctx.stroke(region);
-// };
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.strokeStyle = "grey";
+  ctx.stroke(region);
+};
 
 // Drawing Mesh
 export const drawMesh = (predictions, ctx) => {
@@ -2666,16 +2666,16 @@ export const drawMesh = (predictions, ctx) => {
       const keypoints = prediction.scaledMesh;
 
       // //  Draw Triangles
-      // for (let i = 0; i < TRIANGULATION.length / 3; i++) {
-      //   // Get sets of three keypoints for the triangle
-      //   const points = [
-      //     TRIANGULATION[i * 3],
-      //     TRIANGULATION[i * 3 + 1],
-      //     TRIANGULATION[i * 3 + 2],
-      //   ].map((index) => keypoints[index]);
-      //   //  Draw triangle
-      //   drawPath(ctx, points, true);
-      // }
+      for (let i = 0; i < TRIANGULATION.length / 3; i++) {
+        // Get sets of three keypoints for the triangle
+        const points = [
+          TRIANGULATION[i * 3],
+          TRIANGULATION[i * 3 + 1],
+          TRIANGULATION[i * 3 + 2],
+        ].map((index) => keypoints[index]);
+        //  Draw triangle
+        drawPath(ctx, points, true);
+      }
 
       // Draw Dots
       for (let i = 0; i < keypoints.length; i++) {
