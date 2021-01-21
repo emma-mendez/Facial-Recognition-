26c39195fe77bd95cae2adc132308df2e9fee510
Emma Mendez <emmamendez07@gmail.com>
2021-01-21 10:48:44 +0000

    face detection working in console log and all code perfect at this point

--- src/App.js
+++ src/App.js
@@ -10,6 +10,48 @@ function App() {
   const webcamRef = useRef(null);
 const canvasRef = useRef(null);
 
+// load facemesh
+const runFacemesh = async () =>{
+  const net = await facemesh.load({
+    inputResolution: { 
+      width:640, 
+      height:400, 
+      scale:0.8 }
+  });
+  setInterval(() =>{
+    detect(net)
+  }, 100)
+};
+
+// dection function
+ const detect = async (net) => {
+  if ( 
+    typeof webcamRef.current !=="undefined" && 
+  webcamRef.current !== null && 
+  webcamRef.current.video.readyState === 4
+  ) {
+    // get video props
+const video = webcamRef.current.video;
+const videoWidth = webcamRef.current.video.videoWidth;
+const videoHeight = webcamRef.current.videoHeight;
+
+
+//  setting the video width and canvas width
+webcamRef.current.video.width = videoWidth;
+webcamRef.current.video.height = videoHeight;
+
+canvasRef.current.width = videoWidth;
+canvasRef.current.height = videoHeight;
+
+// Detection
+const face = await net.estimateFaces(video);
+console.log(face);
+
+}
+};
+
+runFacemesh();
+
   return (
     <div className="App">
       <header className="App-header">
